be3e463f92b485106c9106c185adf3e4bb8efb66
Mitraws <m.ws@outlook.com>
2020-07-03 12:15:10 +0200

    Add POST endpoint to create game + add players

---
+++ routers/game.js
@@ -0,0 +1,38 @@
+const { Router } = require("express");
+const Game = require("../models").game;
+const Player = require("../models").player;
+
+const router = new Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const players = req.body.players;
+    if (!players || players.length === 0) {
+      return res.status(400).send("you must provide an array of player names");
+    }
+    const invalid = players.some((name) => !name || name === "");
+    if (invalid) {
+      return res.status(400).send("some players dont have names");
+    }
+
+    const newGame = await Game.create({ name: new Date() });
+
+    const newPlayers = players.map((name) => ({
+      name,
+      gameId: newGame.id,
+      score: 0,
+    }));
+    await Player.bulkCreate(newPlayers);
+
+    return res.status(201).send({ message: "players created" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res) => {
+  const game = await Game.findByPk(1);
+  res.send(game);
+});
+
+module.exports = router;
